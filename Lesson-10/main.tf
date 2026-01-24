fb212f7138d278fa8c609e6bae6b9633b60de551
babaktanriverdi <babaktanriverdi@gmail.com>
2026-01-24 11:30:49 +0100

    modified main.tf

--- Lesson-10/main.tf
+++ Lesson-10/main.tf
@@ -1,80 +1,90 @@
 #----------------------------------------------------------
 # My Terraform
 #
-# Find Latest AMI id of:
-#    - Ubuntu 22.04
-#    - Amazon Linux 2023
-#    - Windows Server 2022 Base
+# Find Latest AMI ID of:
+#   - Ubuntu Server 22.04 LTS
+#   - Amazon Linux 2023
+#   - Windows Server 2022 Base
 #
 # Made by Babak
-#-----------------------------------------------------------
-
+#----------------------------------------------------------
 
 provider "aws" {
   region = "ap-southeast-2"
 }
 
-data "aws_ami" "latest_ubuntu" {
-  owners      = ["099720109477"]
+#----------------------------------------------------------
+# Ubuntu 22.04 LTS (Jammy)
+#----------------------------------------------------------
+data "aws_ami" "latest_ubuntu_2204" {
+  owners      = ["099720109477"] # Canonical
   most_recent = true
+
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
-
-data "aws_ami" "latest_amazon_linux" {
+#----------------------------------------------------------
+# Amazon Linux 2023
+#----------------------------------------------------------
+data "aws_ami" "latest_amazon_linux_2023" {
   owners      = ["amazon"]
   most_recent = true
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["al2023-ami-*-x86_64"]
   }
 }
 
-
-data "aws_ami" "latest_windows_2016" {
+#----------------------------------------------------------
+# Windows Server 2022 Base
+#----------------------------------------------------------
+data "aws_ami" "latest_windows_2022" {
   owners      = ["amazon"]
   most_recent = true
+
   filter {
     name   = "name"
-    values = ["Windows_Server-2016-English-Full-Base-*"]
+    values = ["Windows_Server-2022-English-Full-Base-*"]
   }
-
 }
 
-// How to use
+#----------------------------------------------------------
+# Outputs
+#----------------------------------------------------------
+output "ubuntu_2204_ami_id" {
+  value = data.aws_ami.latest_ubuntu_2204.id
+}
+
+output "ubuntu_2204_ami_name" {
+  value = data.aws_ami.latest_ubuntu_2204.name
+}
+
+output "amazon_linux_2023_ami_id" {
+  value = data.aws_ami.latest_amazon_linux_2023.id
+}
+
+output "amazon_linux_2023_ami_name" {
+  value = data.aws_ami.latest_amazon_linux_2023.name
+}
+
+output "windows_2022_ami_id" {
+  value = data.aws_ami.latest_windows_2022.id
+}
+
+output "windows_2022_ami_name" {
+  value = data.aws_ami.latest_windows_2022.name
+}
+
+#----------------------------------------------------------
+# Example Usage
+#----------------------------------------------------------
 /*
-resource "aws_instance" "my_webserver_with_latest_ubuntu_ami" {
-  ami           = data.aws_ami.latest_ubuntu.id
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.latest_ubuntu_2204.id
   instance_type = "t3.micro"
 }
 */
-
-
-output "latest_windows_2016_ami_id" {
-  value = data.aws_ami.latest_windows_2016.id
-}
-
-output "latest_windows_2016_ami_name" {
-  value = data.aws_ami.latest_windows_2016.name
-}
-
-
-output "latest_amazon_linux_ami_id" {
-  value = data.aws_ami.latest_amazon_linux.id
-}
-
-output "latest_amazon_linux_ami_name" {
-  value = data.aws_ami.latest_amazon_linux.name
-}
-
-
-output "latest_ubuntu_ami_id" {
-  value = data.aws_ami.latest_ubuntu.id
-}
-
-output "latest_ubuntu_ami_name" {
-  value = data.aws_ami.latest_ubuntu.name
-}
